dfd69dc948baa4ca5556e865bda7941e8fd868dc
Honohonoho <751718620@qq.com>
2020-01-11 12:08:32 +0800

    update promise

--- src/promise/index.test.ts
+++ src/promise/index.test.ts
@@ -1,7 +1,9 @@
 import * as chai from 'chai'
+import * as sinon from 'sinon'
+import * as sinonChai from 'sinon-chai'
 import Promise from './index';
-import { createTrue } from 'typescript';
 
+chai.use(sinonChai)
 const assert = chai.assert
 
 describe('Promise', ()=> {
@@ -25,27 +27,150 @@ describe('Promise', ()=> {
     const promise = new Promise(()=>{})
     assert.isFunction(promise.then)
   })
-  it('new Promise(fn) fn 执行时接受 resolve 和 reject 两个函数', ()=> {
-    const promise = new Promise((resolve, reject)=>{
+  it('new Promise(fn) 中的 fn 立即执行', ()=> {
+    let fn = sinon.fake()
+    new Promise(fn)
+    assert(fn.called)
+  })
+  it('new Promise(fn) fn 执行时接受 resolve 和 reject 两个函数',(done)=> {
+    new Promise((resolve, reject)=>{
       assert.isFunction(resolve)
       assert.isFunction(reject)
+      done()
     })
   })
   it('promise.then(success) 中的 success 会在 resolve 时被调用时执行', (done)=> {
-    let called = false
+    let success = sinon.fake()
     const promise = new Promise((resolve, reject)=>{
       // 现在没执行
-      assert(called === false)
+      assert.isFalse(success.called)
       resolve()
       //执行了
       setTimeout(()=>{
-        assert(called === true)
+        assert.isTrue(success.called)
         done()
       })
     })
     // @ts-ignore
-    promise.then(()=> {
-      called = true
+    promise.then(success)
+  })
+  it('promise.then(success) 中的 fail 会在 reject 时被调用时执行', (done)=> {
+    let fail = sinon.fake()
+    const promise = new Promise((resolve, reject)=>{
+      // 现在没执行
+      assert.isFalse(fail.called)
+      reject()
+      //执行了
+      setTimeout(()=>{
+        assert.isTrue(fail.called)
+        done()
+      })
+    })
+    // @ts-ignore
+    promise.then(null, fail)
+  })
+  it('success, fail 必须是个函数,且可选', ()=> {
+    const promise = new Promise((resolve, reject)=>{
+      resolve()
+    })
+    promise.then(false, null)
+    assert(1===1)
+  })
+  it('promise.then(success) 时，状态变为 fulfilled, 且把 promise 的值作为第一个参数', (done)=> {
+    let succeed = sinon.fake()
+    const promise = new Promise((resolve, reject)=>{
+      assert.isFalse(succeed.called)
+      resolve(123)
+      setTimeout(()=>{
+        assert(promise.state === 'fulfilled')
+        assert.isTrue(succeed.called)
+        assert(succeed.calledWith(123))
+        done()
+      })
+    })
+    promise.then(succeed, null)
+  })
+  it('resolve 只能被调用一次', (done)=> {
+    let succeed = sinon.fake()
+    const promise = new Promise((resolve, reject)=>{
+      assert.isFalse(succeed.called)
+      resolve(123)
+      resolve(233)
+      setTimeout(()=>{
+        assert(promise.state === 'fulfilled')
+        assert.isTrue(succeed.calledOnce)
+        assert(succeed.calledWith(123))
+        done()
+      }, 0)
+    })
+    promise.then(succeed, null)
+  })
+  it('reject 只能被调用一次', (done)=> {
+    let fail = sinon.fake()
+    const promise = new Promise((resolve, reject)=>{
+      assert.isFalse(fail.called)
+      reject(123)
+      reject(233)
+      setTimeout(()=>{
+        assert(promise.state === 'rejected')
+        assert.isTrue(fail.calledOnce)
+        assert(fail.calledWith(123))
+        done()
+      }, 0)
+    })
+    promise.then(null, fail)
+  })
+  it('在我的代码执行完之前， 不得调用 then 后面的函数', (done)=> {
+    let succeed = sinon.fake()
+    const promise = new Promise((resolve, reject)=>{
+      assert.isFalse(succeed.called)
+      resolve(123)
+    })
+    promise.then(succeed)
+    assert.isFalse(succeed.called)
+    setTimeout(()=>{
+      assert.isTrue(succeed.called)
+      done()
+    }, 0)
+  })
+  it('promise this 绑定正确', (done)=> {
+    const promise = new Promise((resolve, reject)=>{
+      resolve()
+    })
+    promise.then(function(){
+      "use strict"
+      assert(this === undefined)
+      done()
+    })
+  })
+  it('then 可以在 promise 里多次调用', (done)=> {
+    const promise = new Promise((resolve, reject)=>{
+      resolve()
+    })
+    const callbacks = [sinon.fake(), sinon.fake(), sinon.fake()]
+    promise.then(callbacks[0])
+    promise.then(callbacks[1])
+    promise.then(callbacks[2])
+    setTimeout(()=> {
+      assert(callbacks[0].called)
+      assert(callbacks[1].called)
+      assert(callbacks[2].called)
+      done()
+    })
+  })
+  it('reject 可以在 promise 里多次调用', (done)=> {
+    const promise = new Promise((resolve, reject)=>{
+      reject()
+    })
+    const callbacks = [sinon.fake(), sinon.fake(), sinon.fake()]
+    promise.then(null, callbacks[0])
+    promise.then(null, callbacks[1])
+    promise.then(null, callbacks[2])
+    setTimeout(()=> {
+      assert(callbacks[0].called)
+      assert(callbacks[1].called)
+      assert(callbacks[2].called)
+      done()
     })
   })
 })
--- src/promise/index.ts
+++ src/promise/index.ts
@@ -1,25 +1,45 @@
+import { fail } from "assert"
+
 class Promise2 {
-  succeed = null
-  fail = null
-  resolve() {
-    setTimeout(()=>{
-      this.succeed()
+  state = 'pending'
+  callbacks = []
+  resolve(result) {
+    if (this.state !== 'pending') return
+    this.state = 'fulfilled'
+    setTimeout(() => {
+      this.callbacks.forEach(handle => {
+        if (typeof handle[0] === 'function') {
+          handle[0].call(undefined, result)
+        } 
+      });
     }, 0)
   }
-  reject() {
-    setTimeout(()=>{
-      this.fail()
+  reject(reason) {
+    if (this.state !== 'pending') return
+    this.state = 'rejected'
+    setTimeout(() => {
+      this.callbacks.forEach(handle => {
+        if (typeof handle[1] === 'function') {
+          handle[1].call(undefined, reason)
+        } 
+      });
     }, 0)
   }
   constructor(fn) {
-    if(typeof fn !== "function") {
+    if (typeof fn !== "function") {
       throw new Error('Promise must accept a function')
     }
     fn(this.resolve.bind(this), this.reject.bind(this))
   }
-  then(succeed, fail) {
-    this.succeed = succeed
-    this.fail = fail
+  then(succeed?, fail?) {
+    const handle = []
+    if (typeof succeed === 'function') {
+      handle[0] = succeed
+    }
+    if (typeof fail === 'function') {
+      handle[1] = fail
+    }
+    this.callbacks.push(handle)
   }
 }
 export default Promise2
